7a21dcac910b199ebe8999db316d751529971c4b
Inaki Villar <inaki.seri@gmail.com>
2025-02-04 19:00:38 -0800

    disabling the background upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -49,7 +49,7 @@ develocity {
     server = "https://ge.solutions-team.gradle.com/"
     allowUntrustedServer = true
     buildScan {
-        uploadInBackground.set(true)
+        uploadInBackground.set(false)
         publishing { true }
     }
 }
